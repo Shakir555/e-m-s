fc2ac61044d70023aa5db58c2f8f81e80a8fc868
Shakir Salam <shakirsalam555@Gmail.com>
2025-05-30 23:48:40 +0800

    clean login_dialog.cpp

--- src/login_dialog.cpp
+++ src/login_dialog.cpp
@@ -2,40 +2,38 @@
 #include <QMessageBox>
 #include "login_dialog.h"
 
-LoginDialog::LoginDialog(QWidget *parent)
-    : QDialog(parent), authenticated(false) 
+LoginDialog::LoginDialog(QWidget* parent)
+    : QDialog(parent), authenticated(false)
 {
     setWindowTitle("Login - Employer Mode");
-
     usernameEdit = new QLineEdit(this);
     passwordEdit = new QLineEdit(this);
     passwordEdit->setEchoMode(QLineEdit::Password);
     loginButton = new QPushButton("Login", this);
-
-    QFormLayout *formLayout = new QFormLayout;
+    QFormLayout* formLayout = new QFormLayout;
     formLayout->addRow("Username:", usernameEdit);
     formLayout->addRow("Password:", passwordEdit);
     formLayout->addWidget(loginButton);
-
     setLayout(formLayout);
-
     connect(loginButton, &QPushButton::clicked, this, &LoginDialog::verifyCredentials);
 }
 
-
-void LoginDialog::verifyCredentials() {
+void LoginDialog::verifyCredentials()
+{
     QString username = usernameEdit->text();
     QString password = passwordEdit->text();
-
-    // Hardcoded credentials
-    if (username == "admin" && password == "1234") {
+    if (username == "admin" && password == "1234")
+    {
         authenticated = true;
         accept();
-    } else {
-        QMessageBox::warning(this, "Login Failed", "Invalid username or password.");
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login Failed", "Invalid Username or Password");
     }
 }
 
-bool LoginDialog::isAuthenticated() const {
+bool LoginDialog::isAuthenticated() const
+{
     return authenticated;
 }
