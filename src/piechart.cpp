ec837b3ab3bb61a18025165dd2f6680473287751
Shakir Salam <shakirsalam555@Gmail.com>
2025-06-01 00:06:15 +0800

    clean piechart.cpp

--- src/piechart.cpp
+++ src/piechart.cpp
@@ -1,12 +1,14 @@
-#include "piechart.h"
-#include <QtMath>
 #include <QOpenGLFunctions>
 #include <QPainter>
 #include <QPaintEngine>
+#include <QtMath>
+#include "piechart.h"
 
 PieChartOpenGLWidget::PieChartOpenGLWidget(QWidget* parent)
     : QOpenGLWidget(parent)
-{}
+{
+    //Pie Chart OpenGL Class
+}
 
 void PieChartOpenGLWidget::setDepartmentData(const QMap<QString, int>& data)
 {
@@ -16,7 +18,7 @@ void PieChartOpenGLWidget::setDepartmentData(const QMap<QString, int>& data)
 
 void PieChartOpenGLWidget::initializeGL()
 {
-    glClearColor(0.07f, 0.07f, 0.07f, 1.0f); // grey background
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 void PieChartOpenGLWidget::resizeGL(int w, int h)
@@ -29,28 +31,30 @@ void PieChartOpenGLWidget::paintGL()
     glClear(GL_COLOR_BUFFER_BIT);
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
-
     if (departmentCounts.isEmpty())
+    {
         return;
-
+    }
     QRectF rect(width() / 2 - 150, height() / 2 - 150, 300, 300);
     int total = 0;
-    for (auto count : departmentCounts)
+    for (auto count:departmentCounts)
+    {
         total += count;
-
+    }
     float startAngle = 0.0f;
     int colorIndex = 0;
-    const QColor colors[] = { Qt::red, Qt::green, Qt::blue, Qt::yellow, Qt::cyan, Qt::magenta };
-
-    for (auto it = departmentCounts.begin(); it != departmentCounts.end(); ++it, ++colorIndex) {
+    const QColor colors[] =
+    {
+        Qt::red, Qt::green, Qt::blue, Qt::yellow, Qt::cyan, Qt::magenta
+    };
+    for (auto it = departmentCounts.begin(); it != departmentCounts.end(); ++it, ++colorIndex) 
+    {
         float percentage = static_cast<float>(it.value()) / total;
         float spanAngle = percentage * 360.0f;
-        
-        painter.setPen(QPen(Qt::black, 2));// 2px gold outline
+        painter.setPen(QPen(Qt::black, 2));
         painter.setBrush(colors[colorIndex % 6]);
         painter.drawPie(rect, startAngle * 16, spanAngle * 16);
         startAngle += spanAngle;
     }
-
     painter.end();
 }
